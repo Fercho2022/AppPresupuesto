ab2a588bdc8b2740f9c612b29546236d2e3522c0
fernando <crosio.fernando@gmail.com>
2024-07-09 23:22:05 -0300

    Subo App terminada presupuesto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { GastosComponent } from './components/gastos/gastos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/ingresarpresupuesto', pathMatch: 'full'},
+  {path: 'ingresarpresupuesto', component:IngresarPresupuestoComponent},
+  {path: 'gastos', component:GastosComponent},
+  {path: '**', redirectTo: '/ingresarpresupuesto', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
